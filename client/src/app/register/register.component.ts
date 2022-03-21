c844d57360e2e0a8d9d09fdaea987053180dcd15
Mohammed Ameer <ameerm@sbrgroup.ae>
2022-03-21 14:43:54 +0400

    End of Section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { AccountService } from './../_services/account.service';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+model:any ={};
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+ register(){
+   this.accountService.register(this.model).subscribe(resposne => {
+     console.log(resposne);
+     this.cancel();
+   }, error =>{
+      console.log(error);
+   })
+ }
+
+ cancel(){
+   this.cancelRegister.emit(false);
+ }
+}
